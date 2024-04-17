bc4ec429c5391fab011ff3bf7a9e71c3fa6350bc
qubit07 <qubit07@gmail.com>
2024-04-17 20:59:17 +0200

    add router module

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,19 +4,22 @@ import { AccountService } from '../services/account.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { Router, RouterModule } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, HttpClientModule, CommonModule, NgbDropdownModule],
+  imports: [FormsModule, HttpClientModule, CommonModule, NgbDropdownModule, RouterModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
 
   model: any = {};
 
-  constructor(public accountService: AccountService) {
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {
 
   }
 
@@ -25,15 +28,14 @@ export class NavComponent {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => console.log(error)
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/')
   }
 
 }
