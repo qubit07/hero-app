3223adc909941398039136b95efab6292eef2bea
qubit07 <qubit07@gmail.com>
2024-05-12 11:54:52 +0200

    add gallery for screenshots

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,17 +3,20 @@ import { Member } from '../../models/member';
 import { MembersService } from '../../services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TabsModule, GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
 export class MemberDetailComponent {
 
   member: Member | undefined;
+  images: GalleryItem[] = [];
 
   constructor(private memberService: MembersService, private route: ActivatedRoute) {
     this.loadMember();
@@ -21,13 +24,24 @@ export class MemberDetailComponent {
 
   loadMember() {
     const username = this.route.snapshot.paramMap.get('username');
-    console.log(username)
     if (!username) {
       return;
     }
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: member => {
+        this.member = member;
+        this.getImages();
+
+      }
     })
+  }
+
+  getImages() {
+    if (!this.member) return;
+
+    for (const foto of this.member?.photos) {
+      this.images.push(new ImageItem({ src: foto.url, thumb: foto.url }))
+    }
   }
 
 }
