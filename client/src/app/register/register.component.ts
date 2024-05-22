9e07e3fec278b9dbe8cff32d3c63a4826b549391
qubit07 <qubit07@gmail.com>
2024-05-22 14:46:51 +0200

    add register form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,29 +1,56 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../services/account.service';
+import { TextInputComponent } from '../forms/text-input/text-input.component';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, TextInputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  registerForm: FormGroup = new FormGroup({});
+  validationErrors: string[] | undefined;
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService, private formBuilder: FormBuilder, private router: Router, private toastr: ToastrService) {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : { noMatching: true }
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
+    const values = { ...this.registerForm.value };
+    this.accountService.register(values).subscribe({
       next: () => {
-        this.cancel();
+        this.router.navigateByUrl('/members')
       },
-      error: error => console.log(error)
+      error: error => {
+        this.validationErrors = error
+      }
     })
   }
 
