f10d3774bc6d0a92484ae1bf016d2cb3387b0577
qubit07 <qubit07@gmail.com>
2024-05-23 16:17:11 +0200

    add pagination to client

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -4,20 +4,43 @@ import { MembersService } from '../../services/members.service';
 import { CommonModule } from '@angular/common';
 import { MemberCardComponent } from '../member-card/member-card.component';
 import { Observable } from 'rxjs';
+import { Pagination } from '../../models/pagination';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [CommonModule, MemberCardComponent],
+  imports: [CommonModule, MemberCardComponent, NgbPaginationModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent {
 
-  members$: Observable<Member[]> | undefined;
+  members: Member[] = [];
+  pagination: Pagination | undefined;
+  pageNumber = 1;
+  pageSize = 5;
 
   constructor(private memberService: MembersService) {
-    this.members$ = this.memberService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: response => {
+        if (response.result && response.pagination) {
+          this.members = response.result;
+          this.pagination = response.pagination;
+        }
+      }
+    })
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event) {
+      this.pageNumber = event;
+      this.loadMembers();
+    }
   }
 
 }
